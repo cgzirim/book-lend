6e0554e747b1d4617ca9b443adb84cdd47f1b894
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-09-23 08:28:30 +0100

    Make filter case insensitive

--- frontend_api/api_v1/filters.py
+++ frontend_api/api_v1/filters.py
@@ -1,9 +1,12 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, CharFilter
 
 from api_v1.models import Book
 
 
 class BookFilter(FilterSet):
+    category = CharFilter(field_name="category", lookup_expr="iexact")
+    publisher = CharFilter(field_name="publisher", lookup_expr="iexact")
+
     class Meta:
         model = Book
         fields = ["category", "publisher"]
