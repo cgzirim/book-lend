29cf125a19f76643fc319d0c20fc3f9fac248098
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-09-25 12:14:22 +0100

    Update Swagger doc

--- admin_api/admin_api/settings.py
+++ admin_api/admin_api/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Book-lend API Documentation",
-    "DESCRIPTION": "Frontend API Documentation",
+    "DESCRIPTION": "Admin API Documentation",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
     "COMPONENT_SPLIT_REQUEST": True,
