3d1d80b7cd692b3444846179c786204199f36163
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-09-25 05:33:46 +0100

    Update the authenticate method to closely match authenticate method of the super class

--- admin_api/admin_api/auth_backends.py
+++ admin_api/admin_api/auth_backends.py
@@ -3,13 +3,18 @@ from django.contrib.auth import get_user_model
 
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
-        User = get_user_model()
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        if username is None:
+            email = kwargs.get(UserModel.USERNAME_FIELD)  # USERNAME_FIELD = email
+        if email is None or password is None:
+            return
         try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            return None
-
-        if user.check_password(password):
-            return user
-        return None
+            user = UserModel.objects.get(email=email)
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
+            UserModel().set_password(password)
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
