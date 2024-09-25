b423c1f3bd97c9c9b7786b7efad25acbc206935b
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-09-25 05:38:19 +0100

    Update jwt refresh url pattern, change borrowed_books name

--- admin_api/api_v1/urls.py
+++ admin_api/api_v1/urls.py
@@ -17,9 +17,11 @@ router.register(r"books", BookView, basename="book")
 urlpatterns = [
     path("", include(router.urls)),
     path("users/", ListUsersView.as_view(), name="list-users"),
-    path("borrow/", ListBorrowedBooksView.as_view(), name="borrow-book"),
+    path(
+        "borrowed_books/", ListBorrowedBooksView.as_view(), name="list-borrowed-books"
+    ),
     path("admin/register/", RegisterView.as_view(), name="register-user"),
     path("admin/login/", LoginView.as_view(), name="login"),
     path("admin/logout/", LogoutView.as_view(), name="logout"),
-    path("admin/refresh_jwt/", JWTRefreshView.as_view(), name="refresh_jwt"),
+    path("admin/refresh/token/", JWTRefreshView.as_view(), name="refresh_jwt"),
 ]
