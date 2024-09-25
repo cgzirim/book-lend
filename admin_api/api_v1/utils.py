055926e02663265bbcc58d92dccdb77bf8789d34
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-09-25 05:39:20 +0100

    Write function make drf seriaizer data json serializable

--- admin_api/api_v1/utils.py
+++ admin_api/api_v1/utils.py
@@ -1,7 +1,39 @@
+from uuid import UUID
+from decimal import Decimal
 from django.db import models
+from datetime import datetime, date
 
 
 class LowercaseCharField(models.CharField):
     def get_prep_value(self, value):
         value = super().get_prep_value(value)
         return value if value is None else value.lower()
+
+
+def convert_to_serializable(data):
+    """
+    Recursively converts non-serializable fields like datetime and Decimal to strings.
+
+    Args:
+        data (dict, list, or primitive types): The data to process.
+
+    Returns:
+        The data with all non-serializable fields converted to strings.
+    """
+    if isinstance(data, dict):
+        return {key: convert_to_serializable(value) for key, value in data.items()}
+
+    elif isinstance(data, list):
+        return [convert_to_serializable(item) for item in data]
+
+    elif isinstance(data, (datetime, date)):
+        return data.isoformat()
+
+    elif isinstance(data, Decimal):
+        return str(data)
+
+    elif isinstance(data, UUID):
+        return str(data)
+
+    else:
+        return data
