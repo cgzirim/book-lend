49fd1d2959ed5ccc2b69558ec526981b8aa82994
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-09-25 10:24:41 +0100

    Modify refresh token url path

--- frontend_api/api_v1/urls.py
+++ frontend_api/api_v1/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path("user/register/", RegisterView.as_view(), name="register-user"),
     path("user/login/", LoginView.as_view(), name="login"),
     path("user/logout/", LogoutView.as_view(), name="logout"),
-    path("user/refresh_jwt/", JWTRefreshView.as_view(), name="refresh_jwt"),
+    path("user/refresh/token/", JWTRefreshView.as_view(), name="refresh_jwt"),
 ]
