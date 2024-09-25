ce0c03b6ebbcab019ca3dbc5111988682df27a75
Chigozirim Igweamaka <cgzirim@gmail.com>
2024-09-25 05:35:14 +0100

    Register signals

--- admin_api/api_v1/apps.py
+++ admin_api/api_v1/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ApiV1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_v1'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api_v1"
+
+    def ready(self):
+        import api_v1.signals
